212e460e4ffab8363f828c8c3e108de60cfbc1b9
Aaron Juarez <aarjuamtz@gmail.com>
2016-07-02 12:55:32 -0500

    Test: 'Initial Entries' and 'New Feed Selection' Suites

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -94,17 +94,57 @@ $(function() {
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
+    describe('Initial Entries', function() {
+
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        it('at least one', function(done) {
+            var entry = document.getElementsByClassName('entry');
+            expect(entry.length).not.toBe(0);
+            done();
+        });
+    });
+
+    /* TODO: Write a new test suite named "New Feed Selection"*/
+
+    describe('New Feed Selection', function() {
+        var headerTitle,
+            firstFeed,
+            newHeaderTitle,
+            newFirstFeed;
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         beforeEach(function(done) {
+            loadFeed(2, function(){
+                headerTitle = document.getElementsByClassName('header-title')[0].textContent;
+                firstFeed = document.getElementsByClassName('entry')[0].textContent;
+                done();
+            });
+         });
+
+         it('loads new feed', function(done) {
+            loadFeed(0, function(){
+                newHeaderTitle = document.getElementsByClassName('header-title')[0].textContent;
+                newFirstFeed = document.getElementsByClassName('entry')[0].textContent;
+                expect(headerTitle).not.toEqual(newHeaderTitle);
+                expect(firstFeed).not.toEqual(newFirstFeed);
+                done();
+            });
+
+         });
+
+    });
+
+
 }());
