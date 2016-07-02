6f934f023685991c5788cef8b2e89595f947717d
Aaron Juarez <aarjuamtz@gmail.com>
2016-07-02 14:49:51 -0500

    Chore: Dist structure added

---
+++ Gruntfile.js
@@ -0,0 +1,80 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    uglify: {
+      js: {
+        files: {
+          'dist/js/app.js': ['src/js/app.js'],
+          'dist/jasmine/lib/jasmine-2.1.2/boot.js': ['src/jasmine/lib/jasmine-2.1.2/boot.js'],
+          'dist/jasmine/lib/jasmine-2.1.2/console.js': ['src/jasmine/lib/jasmine-2.1.2/console.js'],
+          'dist/jasmine/lib/jasmine-2.1.2/jasmine-html.js': ['src/jasmine/lib/jasmine-2.1.2/jasmine-html.js'],
+          'dist/jasmine/lib/jasmine-2.1.2/jasmine.js': ['src/jasmine/lib/jasmine-2.1.2/jasmine.js'],
+          'dist/jasmine/spec/feedreader.js': ['src/jasmine/spec/feedreader.js']
+        }
+      }
+    },
+
+    htmlmin: {                                     // Task
+      main: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'dist/index.html': 'src/index.html'         // 'destination': 'source'
+        }
+      }
+    },
+
+    cssmin: {
+      css: {
+        files: [{
+          expand: true,
+          cwd: 'src/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/css',
+          ext: '.css'
+        },
+        {
+          expand: true,
+          cwd: 'src/jasmine/lib/jasmine-2.1.2',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/jasmine/lib/jasmine-2.1.2',
+          ext: '.css'
+        }]
+      }
+    },
+
+    pagespeed: {
+      options: {
+        nokey: true,
+        url: "http://aaronjuarez.github.io/feed-reader-testing/"
+      },
+      prod: {
+        options: {
+          url: "http://aaronjuarez.github.io/feed-reader-testing/",
+          locale: "en_GB",
+          strategy: "desktop",
+          threshold: 80
+        }
+      },
+      paths: {
+        options: {
+          paths: [],
+          locale: "en_GB",
+          strategy: "desktop",
+          threshold: 80
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin']);
+  grunt.registerTask('html', ['htmlmin']);
+  grunt.registerTask('speed', ['pagespeed']);
+
+};
--- README.md
+++ README.md
@@ -1,7 +1,50 @@
 # My notes
+
 ## To run
 - Download or fork the application from [here](https://github.com/AaronJuarez/feed-reader-testing)
 - Select ```index.html``` to start the app
+
+### Running Grunt
+----------------------------
+[Installing Grunt](http://gruntjs.com/getting-started)
+
+You will also need to install the following plugings:
+
+- [grunt-contrib-uglify](https://github.com/gruntjs/grunt-contrib-uglify)
+
+```
+npm install grunt-contrib-uglify --save-dev
+```
+
+- [grunt-contrib-cssmin](https://github.com/gruntjs/grunt-contrib-cssmin)
+
+```
+npm install grunt-contrib-cssmin --save-dev
+```
+
+- [grunt-contrib-htmlmin](https://github.com/gruntjs/grunt-contrib-htmlmin)
+
+```
+npm install grunt-contrib-htmlmin --save-dev
+```
+
+- [grunt-pagespeed](https://www.npmjs.com/package/grunt-pagespeed)
+
+```
+npm install grunt-pagespeed --save-dev
+```
+
+Alternatively use load-grunt-tasks to load your plugins.
+
+- [load-grunt-tasks](https://github.com/sindresorhus/load-grunt-tasks)
+
+```
+npm install --save-dev load-grunt-tasks
+```
+
+```
+require('load-grunt-tasks')(grunt);
+```
 
 # Project Overview
 
